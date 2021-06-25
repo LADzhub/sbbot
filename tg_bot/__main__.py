d3ba103684ec17bd04463094337a7d32774f3763
holymwon <85745878+holymwon@users.noreply.github.com>
2021-06-25 11:02:36 +0530

    Update__main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -26,9 +26,9 @@ from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 
 PM_START_TEXT = """
-**Hello {}, My Name is {}!** 
-I am an **SUPERB**  group management bot.
-You can find the list of available commands with /help.
+**ഹലോ! {},ഞാൻ {}!** 
+ഞാൻ ഒരു **SUPERB**  group_management_bot ആണ്.
+Find the list of available commands with /help.
 
 """
 
@@ -139,11 +139,11 @@ def start(bot: Bot, update: Update, args: List[str]):
             update.effective_message.reply_photo(
                 TECHNO_IMG,
                 PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
-                parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="🤝HELP🤝",
+                parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="HELP",
                                                                        callback_data="help_back".format(bot.username)),
-                                                                                   InlineKeyboardButton(text="🧑‍💻My Creator🧑‍💻",
+                                                                                   InlineKeyboardButton(text="MY B0SS",
                                                                        url="t.me/alavalaathy")],
-                                                                                   [InlineKeyboardButton(text="ADD 《ᗷ卄ΛᔕIKKΛ B0T》TO YOUR GROUP",
+                                                                                   [InlineKeyboardButton(text="ADD《ᗷ卄ΛᔕIKKΛ B0T》TO YOUR GROUP",
                                                                        url="t.me/{}?startgroup=true".format(bot.username)),
                                                                                    InlineKeyboardButton(text="Source Code",
                                                                        url="https://github.com/holymwon/sbbot")
